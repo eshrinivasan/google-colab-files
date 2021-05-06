c6f6e3114248767be7f4ab8a0ff0525001c485fd
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2021-05-06 15:18:07 +0530

    2nd level training from hugging face mlm

---
+++ 2_run_mlm_pred.py
@@ -0,0 +1,25 @@
+import sys
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+import torch
+
+useSecondLevelPretraining = sys.argv[1] or True
+input_text = sys.argv[2] or "this [MASK] is quite reflective"
+
+
+if(useSecondLevelPretraining):
+  tok = AutoTokenizer.from_pretrained("./train_data/checkpoint-1500")
+  model = AutoModelForMaskedLM.from_pretrained("./train_data/checkpoint-1500")
+else:
+  tok = AutoTokenizer.from_pretrained("bert-large-uncased")
+  model = AutoModelForMaskedLM.from_pretrained("bert-large-uncased")
+
+x = tok.encode(input_text)
+idx = x.index(103)
+x = torch.Tensor([x]).long()
+
+l = model(x)[0]
+l = l[0][idx].detach().cpu().numpy()
+la = l.argsort()
+
+for i in range(1, 11):
+    print(tok.decode([la[-i]]))
--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # google-colab-files
 
+
+1. Run the python file for training bert with domain specific data
+python 1_run_bert_2ndlevel_trainer.py --train_file "./output_5.txt" --model_name_or_path bert-large-uncased --do_train --output_dir ./train_data --line_by_line True
+
+2. Run the python file for prediction of domain specific masked word
+python 2_run_mlm_pred.py False "I absolutely love the [MASK]"
+
+
+### Reference:
+
 run_mlm.py from https://github.com/huggingface/transformers/blob/master/examples/pytorch/language-modeling/run_mlm.py
 
 https://stackoverflow.com/questions/64712375/fine-tune-bert-for-specific-domain-unsupervised
+
+
